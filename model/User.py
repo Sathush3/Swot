03207114c2226c8f8a45794002c7a577470a1c3d
Sathurshan Ramachandran <61341190+Sathush3@users.noreply.github.com>
2020-05-20 04:51:21 +0530

    Create User.py

---
+++ model/User.py
@@ -0,0 +1,8 @@
+class User:
+
+    def __init__(self ,name, email, password):
+        self.name = name 
+        self.email = email
+        self.password = password
+        self.verified = False
+        self.active = False
